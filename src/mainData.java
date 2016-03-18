46ec352da66678e0d5bdca94cbb53649eaa7c27b
Salma Hanifah <salmahnfh98@gmail.com>
2016-03-18 11:49:18 +0700

    membuat main class dengan nama mainData untuk mencoba method get() apakah dapat berjalan sesuai yang diinginkan

---
+++ src/mainData.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas_2;
+
+/**
+ *
+ * @author salmahnfh
+ */
+public class mainData {
+    public static void main(String[] args) {
+        Data db=new Data();
+        db.get();
+    }
+}
