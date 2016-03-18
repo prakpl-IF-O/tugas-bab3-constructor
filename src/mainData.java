057857d91efda180a6cf47f152bd0775e01ed84f
Salma Hanifah <salmahnfh98@gmail.com>
2016-03-18 22:18:12 +0700

    penambahan keterangan untuk outputnya

--- src/mainData.java
+++ src/mainData.java
@@ -12,10 +12,15 @@ package Tugas_2;
 public class mainData {
     public static void main(String[] args) {
         Data db=new Data();
+        System.out.println("ket : method db.get()");
         db.get();//mengambil semua data
+        System.out.println("ket : method db.get(104)");
         db.get(104);//mengambil data dengan nik/id=104
+        System.out.println("ket : method db.get(\"gaji pokok\",2000");
         db.get("gaji pokok",2000);//mengambil semua data yang memiliki gaji pokok=2000
+        System.out.println("ket : method db.get(\"tunjangan\",\"<=\",1000)");
         db.get("tunjangan","<=",1000);//mengambil semua data yang memiliki tunjangan<=1000
+        System.out.println("ket : method db.get(\"bonus\",\">\",450)");
         db.get("bonus",">",450);//mengambil semua data yang memiliki bonus<450
     }
 }
