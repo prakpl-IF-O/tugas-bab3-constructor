760349abaa71f9be6f0fcafc5d80a1d643bbb592
Faza Abdi Perkasa <abdifaza@yahoo.com>
2016-03-19 14:24:51 +0700

    pembuatan class dataBase dan pembuatan overloading method sesuai ketentuan

---
+++ src/dataBase/dataBase.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataBase;
+
+/**
+ *
+ * @author Faza Abdi
+ */
+public class dataBase {
+    public void get(){
+    
+    }
+    public void get(int id){
+    
+    }
+    public void get(String columnName, int obValue){
+    
+    }
+    public void get(String columnName, String operator, int obValue){
+    
+    }
+}
