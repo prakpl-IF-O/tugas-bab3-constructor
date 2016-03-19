49c69c25d079fe35e9e15d156f17665eaf264642
pritanrf <pritafaridiani@yahoo.co.id>
2016-03-19 23:12:32 +0700

    main data

--- src/mainTugas.java
+++ src/mainTugas.java
@@ -5,6 +5,10 @@ public class mainTugas {
         Tugas t = new Tugas();
         
         t.get();
+        t.get(104);
+        t.get("GAJI POKOK",2000);
+        t.get("TUNJANGAN","<=",1000);
+        t.get("BONUS",">",500);
     }
     
 }
