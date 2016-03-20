9fe00da61d473274e0197f6b6818603975a1fef5
Reza Faridh Mashyuri <refamasfuma@gmail.com>
2016-03-20 20:34:12 +0700

    Maaf sam,.. Abis makan dari luar,.. ini Baru bikin Objek sama Method Class set, get, dan juga untuk Outputin karyawanya :D

--- src/MainBab3.java
+++ src/MainBab3.java
@@ -1,6 +1,16 @@
 // Bismillah awali dengan doa 
 public class MainBab3 {
     public static void main(String[] args) {
+        Data_Karyawan k1 = new Data_Karyawan ("Paijo",101,2000,1000,500,200);
+        Data_Karyawan k2 = new Data_Karyawan ("Lala",102,1750,900,500,200);
+        Data_Karyawan k3 = new Data_Karyawan ("Lulu",103,2150,1000,300,375);
+        Data_Karyawan k4 = new Data_Karyawan ("Sabar",104,1500,750,600,200);
+        Data_Karyawan k5 = new Data_Karyawan ("Sule",105,2500,850,500,300);
+        Data_Karyawan k6 = new Data_Karyawan ("Paiman",106,1500,350,450,250);
+        Data_Karyawan k7 = new Data_Karyawan ("Paninim",107,2000,1000,500,200);
+        Data_Karyawan k8 = new Data_Karyawan ("Sutiman",108,1500,1250,500,300);
+        Data_Karyawan k9 = new Data_Karyawan ("Raja",109,1000,1000,500,200);
+        Data_Karyawan k10 = new Data_Karyawan ("Harry",101,2250,1000,200,200);
         
     }
 }
