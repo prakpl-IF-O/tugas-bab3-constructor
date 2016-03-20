c9a6e20f2df65f14db8ff3764091df930e55061e
Dwiki Prayogo Wicaksono <dwikiprayogo97@gmail.com>
2016-03-20 16:06:19 +0700

    Pembuatan class Database.java dan struktur data

---
+++ src/Database.java
@@ -0,0 +1,38 @@
+public class Database {
+    private int nik;
+    private long gajiPokok, tunjangan, bonus, denda;
+    private String nama;
+
+    public Database(int nik, String nama, long gajiPokok, long tunjangan, long bonus, long denda) {
+        this.nik = nik;
+        this.gajiPokok = gajiPokok;
+        this.tunjangan = tunjangan;
+        this.bonus = bonus;
+        this.denda = denda;
+        this.nama = nama;
+    }
+
+    public int getNik() {
+        return nik;
+    }
+
+    public long getGajiPokok() {
+        return gajiPokok;
+    }
+
+    public long getTunjangan() {
+        return tunjangan;
+    }
+
+    public long getBonus() {
+        return bonus;
+    }
+
+    public long getDenda() {
+        return denda;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+}
