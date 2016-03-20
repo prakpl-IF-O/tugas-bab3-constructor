0968c9da3e72a3389bdfcbf2da510b2fd1c96261
Husein Abdulbar <abdulbarhusein@yahoo.com>
2016-03-20 19:52:58 +0700

    new

---
+++ src/data.java
@@ -0,0 +1,32 @@
+
+public class data {
+
+    public data() {
+        String m[] = {"nik", "nama", "gaji pokok", "tunjangan", "bonus", "denda"},
+             nik[] = {"1","2","3","4","5","6","7","8","9","10"},
+               n[] = {"","","","","","","","","",""};
+        int g[] = {2000,2500,1500,2000,1250,2700,3000,2000,1500,2500},
+            t[] = {1000,900,750,1000,800,1000,900,1250,750,500},
+            b[] = {500,500,500,500,500,500,500,500,500,500},
+            d[] = {500,500,500,500,500,500,500,500,500,500,};
+
+    }
+
+    public void get() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+
+    public void get(int id) {
+
+    }
+
+    public void get(String data, int v) {
+
+    }
+
+    public void get(String d, String m, int v) {
+
+    }
+}
