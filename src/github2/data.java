4f745c998fb14476d8bd4a9b0b2e74f441390ca3
chelsafarah <chelsafv75@gmail.com>
2016-03-20 23:59:25 +0700

    tambah method

--- src/github2/data.java
+++ src/github2/data.java
@@ -29,15 +29,97 @@ public class data {
     public void get(int id){
         System.out.println("***********DATA KARYAWAN BERDASARKAN NIK "+id+"***********");
         for(int i=0;i<nama.length;i++){
-        if(id==database[i][0]){
-            System.out.println("Nik\t\t: "+database[i][0]);
-            System.out.println("Nama\t\t: "+nama[i]);
-            System.out.println("Gaji Pokok\t: "+database[i][1]);
-            System.out.println("Tunjangan\t: "+database[i][2]);
-            System.out.println("bonus\t\t: "+database[i][3]);
-            System.out.println("denda\t\t: "+database[i][4]);
+            if(id==database[i][0]){
+                System.out.println("Nik\t\t: "+database[i][0]);
+                System.out.println("Nama\t\t: "+nama[i]);
+                System.out.println("Gaji Pokok\t: "+database[i][1]);
+                System.out.println("Tunjangan\t: "+database[i][2]);
+                System.out.println("bonus\t\t: "+database[i][3]);
+                System.out.println("denda\t\t: "+database[i][4]);
+            }
+        }System.out.println("\n");
+    }
+    public void get(String columnName, int value){
+        System.out.println("********************Data Karyawan dengan "+columnName+" "+value+"********************\n");
+        System.out.printf("%s %9s %17s %15s %12s %15s \n","Nik","Nama","Gaji Pokok","Tunjangan","Bonus","Denda");
+        int k = 0;
+        if("Gaji Pokok".equalsIgnoreCase(columnName)){
+            k=1;
+        }else if("Tunjangan".equalsIgnoreCase(columnName)){
+            k=2;
+        }else if("Bonus".equalsIgnoreCase(columnName)){
+            k=3;
+        }else if("Denda".equalsIgnoreCase(columnName)){
+            k=4;
+        }
+        for(int i=0;i<nama.length;i++){
+            if(value==database[i][k]){
+                System.out.print(database[i][0]+"\t");
+                System.out.print(nama[i]+"\t\t");
+                for (int j=1;j<5;j++){
+                    System.out.print(database[i][j]+"\t\t");
+                }System.out.println();
+            }
+        }System.out.println("\n");
+    }
+    public void get(String columnName, String operator, int value){
+        System.out.println("********************Data Karyawan dengan "+columnName+" "+operator+" "+value+"********************\n");
+        System.out.printf("%s %9s %17s %15s %12s %15s \n","Nik","Nama","Gaji Pokok","Tunjangan","Bonus","Denda");
+        int m = 0;
+        if("Gaji Pokok".equalsIgnoreCase(columnName)){
+            m=1;
+        }else if("Tunjangan".equalsIgnoreCase(columnName)){
+            m=2;
+        }else if("Bonus".equalsIgnoreCase(columnName)){
+            m=3;
+        }else if("Denda".equalsIgnoreCase(columnName)){
+            m=4;
+        }
+        switch (operator){
+            case "<":
+                for(int i=0;i<nama.length;i++){
+                    if(database[i][m]<value){
+                        System.out.print(database[i][0]+"\t");
+                        System.out.print(nama[i]+"\t\t");
+                        for (int j=1;j<5;j++){
+                            System.out.print(database[i][j]+"\t\t");
+                        }System.out.println();
+                    }
+                }
+                break;
+            case "<=":
+                for(int i=0;i<nama.length;i++){
+                    if(database[i][m]<=value){
+                        System.out.print(database[i][0]+"\t");
+                        System.out.print(nama[i]+"\t\t");
+                        for (int j=1;j<5;j++){
+                            System.out.print(database[i][j]+"\t\t");
+                        }System.out.println();
+                    }
+                }
+                break;
+            case ">":
+                for(int i=0;i<nama.length;i++){
+                    if(database[i][m]>value){
+                        System.out.print(database[i][0]+"\t");
+                        System.out.print(nama[i]+"\t\t");
+                        for (int j=1;j<5;j++){
+                            System.out.print(database[i][j]+"\t\t");
+                        }System.out.println();
+                    }
+                }
+                break;
+            case ">=":
+                for(int i=0;i<nama.length;i++){
+                    if(database[i][m]>=value){
+                        System.out.print(database[i][0]+"\t");
+                        System.out.print(nama[i]+"\t\t");
+                        for (int j=1;j<5;j++){
+                            System.out.print(database[i][j]+"\t\t");
+                        }System.out.println();
+                    }
+                }
+                break;
         }
     }
-    }
-    
 }
--- src/github2/dataMain.java
+++ src/github2/dataMain.java
@@ -4,6 +4,8 @@ public class dataMain {
     public static void main(String[] args) {
         data d1 = new data();
         d1.get();
-        d1.get(102);
+        d1.get(104);
+        d1.get("Gaji Pokok", 1750);
+        d1.get("Bonus",">",450);
     }
 }
