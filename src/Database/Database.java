c3a30193a0bea44de345bc7aafc108ed5cd1bc35
Andi Rosadi <andymavia45@gmail.com>
2016-03-20 09:54:43 +0700

    Penambahan method get () untuk  mengambil semua data dari data Base dan penambahan method get(int ID) untuk mengambil salah satu databes berdasarkan ID/NIK

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -7,5 +7,22 @@ public class Database {
     private int Tunjangan [] = {1000, 900, 1000, 750, 850, 350, 1000, 1250, 1000, 1000};
     private int Bonus [] = {500, 500, 300, 600, 500, 450, 500, 500, 500, 200};
     private int Denda [] = {200, 200, 375, 200,300, 250, 200, 300, 200, 200};
+    
+    public void get(){
+        System.out.println("\t \tDATABASE KARYAWAN \n=============================================");
+        System.out.printf("%s%15s%15s%15s%15s%15s\n","NIK", "Nama", "Gaji Pokok", "Tunjangan", "Bonus", "Denda");
+        for (int a=0; a<NIK.length; a++){
+            System.out.printf("%d%15s%15d%15d%15d%15d\n",NIK[a],Nama[a],GajiPokok[a],Tunjangan[a],Bonus[a],Denda[a]);
+        }System.out.println();
+    }
+    public void get (int ID){ //Untuk mengambil salah satu data berdasarkan id
+        System.out.println("\t \tDATABASE KARYAWAN DILIHAT DARI ID\n=============================================");
+        System.out.printf("%s%15s%15s%15s%15s%15s\n","NIK", "Nama", "Gaji Pokok", "Tunjangan", "Bonus", "Denda");
+        for (int a=0; a<NIK.length; a++){
+        if (NIK[a]==ID){
+                System.out.printf("%d%15s%15d%15d%15d%15d\n",NIK[a],Nama[a],GajiPokok[a],Tunjangan[a],Bonus[a],Denda[a]);
+        }
+        }System.out.println();
+    }
 }
 }
