8edb173daa0888d5516e10c49da20d7ebeedeb36
suastikaadinata <suastikaadinata97@gmail.com>
2016-03-21 06:43:21 +0700

    Selesai membuat sebuah contructor Database untuk database yang akan dibuat nanti pada class Database

--- src/Database.java
+++ src/Database.java
@@ -3,11 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//"nik":101,
+//      "nama":"Paijo",
+//      "gajiPokok":2000,
+//      "tunjangan":1000,
+//      "bonus":500,
+//      "denda":200
 
 /**
  *
  * @author gunka
  */
 public class Database {
-    
+
+    String nama;
+    int nik, gajiPokok, tunjangan, bonus, denda;
+
+    Database(int nik, String nama, int gajiPokok, int tunjangan, int bonus, int denda) {
+        this.nik = nik;
+        this.nama = nama;
+        this.gajiPokok = gajiPokok;
+        this.tunjangan = tunjangan;
+        this.bonus = bonus;
+        this.denda = denda;
+    }
+
 }
