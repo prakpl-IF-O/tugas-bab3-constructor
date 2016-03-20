4d16556e7bef6dd2e6c501e174155d61d2ac9aec
Dino Keylas <dinokeylas@gmail.com>
2016-03-20 14:00:42 +0700

    Membuat constructor dan method get pada classSimpanGaji

--- src/Tugas2GitHub/MainGaji.java
+++ src/Tugas2GitHub/MainGaji.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Tugas2GitHub;
-
-/**
- *
- * @author Dino Keylas
- */
 public class MainGaji {
     
 }
--- src/Tugas2GitHub/SimpanGaji.java
+++ src/Tugas2GitHub/SimpanGaji.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Tugas2GitHub;
-
-/**
- *
- * @author Dino Keylas
- */
 public class SimpanGaji {
-    
+   private String nama;
+   private int nik,gajiPokok,tunjangan,bonus,denda;
+
+    public SimpanGaji(String nama, int nik, int gajiPokok, int tunjangan, int bonus, int denda) {
+        this.nama = nama;
+        this.nik = nik;
+        this.gajiPokok = gajiPokok;
+        this.tunjangan = tunjangan;
+        this.bonus = bonus;
+        this.denda = denda;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getNik() {
+        return nik;
+    }
+
+    public int getGajiPokok() {
+        return gajiPokok;
+    }
+
+    public int getTunjangan() {
+        return tunjangan;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public int getDenda() {
+        return denda;
+    }
 }
