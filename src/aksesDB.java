b06e160747d4e645d39aa8b7825b93035acff67a
Dwiki Prayogo Wicaksono <dwikiprayogo97@gmail.com>
2016-03-20 16:25:00 +0700

    Pembuatan class aksesDB.java dan pembuatan data pada class aksesDB.java dengan instansiasi ke Database.java

---
+++ src/aksesDB.java
@@ -0,0 +1,17 @@
+public class aksesDB {
+    private static Database[] db = new Database[10];
+    //Data
+    public static void data(){
+        db[0] = new Database(101, "Paijo", 2000, 1000, 500, 200);
+        db[1] = new Database(102, "Lala", 1750, 900, 500, 200);
+        db[2] = new Database(103, "Lulu", 2150, 1000, 300, 375);
+        db[3] = new Database(104, "Sabar", 1500, 750, 600, 200);
+        db[4] = new Database(105, "Sule", 2500, 850, 500, 300);
+        db[5] = new Database(106, "Paiman", 1500, 350, 450, 250);
+        db[6] = new Database(107, "Ponimin", 2000, 1000, 500, 200);
+        db[7] = new Database(108, "Sutiman", 1500, 1250, 500, 300);
+        db[8] = new Database(109, "Raja", 1000, 1000, 500, 200);
+        db[9] = new Database(110, "Harry", 2250, 1000, 200, 200);
+    }
+    
+}
