6043113f149c2562b202fd63cb1c3bb8722e9962
Andi Rosadi <andymavia45@gmail.com>
2016-03-20 09:43:47 +0700

    Pembuatan Package baru dengan nama Database disertati dengan  pembuatan java class dengan nama Database dan mainDatabase, dan pembuatan class ID pada java class Database

---
+++ src/Database/Database.java
@@ -0,0 +1,11 @@
+package Database;
+public class Database { 
+    public class ID {
+    private int NIK [] = {101, 102, 103, 104, 105, 106, 107, 108, 109, 110};
+    private String Nama[] = {"Paijo", "Lala", "Lulu", "Sabar", "Sule", "Paiman", "Ponimin", "Sutiman", "Raja", "Harry"};
+    private int GajiPokok [] = {2000, 1750, 2150, 1500, 2500, 1500, 2000, 1500, 1000, 2250};
+    private int Tunjangan [] = {1000, 900, 1000, 750, 850, 350, 1000, 1250, 1000, 1000};
+    private int Bonus [] = {500, 500, 300, 600, 500, 450, 500, 500, 500, 200};
+    private int Denda [] = {200, 200, 375, 200,300, 250, 200, 300, 200, 200};
+}
+}
