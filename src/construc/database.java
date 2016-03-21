474071b42f525653113642a11eedb197c3ec23d0
Muhammad Bayu Lesmana <bayyulesmana22@gmail.com>
2016-03-21 22:17:19 +0700

    membuatt methodddddd

--- src/construc/database.java
+++ src/construc/database.java
@@ -122,3 +122,28 @@ public class database {
                 }
             }
         }
+  if(operator.equals("<=")){
+            System.out.println("NIK     NAMA    GajiPokok   Tunjangan   Bonus   Denda");
+            for (int i = 0; i < 10; i++) {
+                if (tunjangan[i] <= value) {
+                    System.out.printf("%-7d %-9s %-11d %-10d %-8d %d\n", nik[i], nama[i], gajiPokok[i], tunjangan[i], bonus[i], denda[i]);
+                }
+            }
+        }
+            if(operator.equals(">")){
+            System.out.println("NIK     NAMA    GajiPokok   Tunjangan   Bonus   Denda");
+            for (int i = 0; i < 10; i++) {
+                if (tunjangan[i] > value) {
+                    System.out.printf("%-7d %-9s %-11d %-10d %-8d %d\n", nik[i], nama[i], gajiPokok[i], tunjangan[i], bonus[i], denda[i]);
+                }
+            }
+        }
+            if(operator.equals(">=")){
+            System.out.println("NIK     NAMA    GajiPokok   Tunjangan   Bonus   Denda");
+            for (int i = 0; i < 10; i++) {
+                if (tunjangan[i] >= value) {
+                    System.out.printf("%-7d %-9s %-11d %-10d %-8d %d\n", nik[i], nama[i], gajiPokok[i], tunjangan[i], bonus[i], denda[i]);
+                }
+            }
+        }
+    }
