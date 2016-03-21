99eea88503e4dcdd275ebc5027b568d04a23be3a
muhammadiqbalaulia <iqbalgendon21@gmail.com>
2016-03-21 21:27:01 +0700

    penambahan class data dengan parameter

--- src/data.java
+++ src/data.java
@@ -7,4 +7,12 @@ public class data {
     private int[] bonus = new int[10];
     private int[] denda = new int[10];
     int urut=0;
-    
+    public void data(String nama_,int nik_, int gapok_, int tunjangan_, int bonus_, int denda_) {
+        nama[urut] = nama_;
+        nik[urut] = nik_;
+        gapok[urut] = gapok_;
+        tunjangan[urut] = tunjangan_;
+        bonus[urut] = bonus_;
+        denda[urut] = denda_;
+        urut++;
+    }}
