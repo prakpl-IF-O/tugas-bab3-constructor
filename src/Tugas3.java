224d163ba321b2b97f3c86ea3c97d6cd21e3f8d6
Fariz Fathullah Afma <farizafma@yahoo.co.id>
2016-03-21 23:58:24 +0700

    pembuatan class

---
+++ src/Tugas3.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fariz
+ */
+public class Tugas3 {
+     public static void main(String[] args) {
+            public static void main(String[] args) {
+    ClassBaru x1 = new ClassBaru();
+    x1.masukData("101","paijo",2000,1000,500,200);
+    x1.masukData("102","Lala",1750,900,500,200);
+    x1.masukData("103","Lulu",2150,1000,300,375);
+    x1.masukData("104","Sabar",1500,750,600,200);
+    x1.masukData("105","Sule",2500,850,500,300);
+    x1.masukData("106","Paiman",1500,350,450,250);
+    x1.masukData("107","Ponimin",2000,1000,500,200);
+    x1.masukData("108","Sutiman",1500,1250,500,300);
+    x1.masukData("109","Raja",1000,1000,500,200);
+    x1.masukData("110","Harry",2250,1000,200,20);
+    x1.get();
+    x1.get("104");
+    x1.get("tunjangan",2000);
+    x1.get("tunjangan","<=",1000);
+    x1.get("bonus",">" , 450);
+    }
+    
+    }
+    
+}
