c4e3b34daa5a55370d1f0da871f3048f668602f1
Jerry Kurnia Sandy <jerrykurnias@gmail.com>
2016-03-21 11:29:41 +0700

    Perombakan Program. Dapat memanggil semua Data Karyawan

--- src/Karyawan.java
+++ src/Karyawan.java
@@ -1,68 +1,48 @@
 public class Karyawan {
-    private int NIK, Gajipokok, Tunjangan, Bonus, Denda;
-    private String NamaKar;
-public Karyawan (){
-}    
-//Konstraktor
-    public void Karyawan(int q, String w, int e,int r, int t, int y ) {
-        NIK       = q;
-        NamaKar   = w;
-        Gajipokok = e;
-        Tunjangan = r;
-        Bonus     = t;
-        Denda     = y;
-        
-    }
-//set
-    public void setNIK(int q) {
-        NIK = q;
-    }
-    public void setGajipokok(int e) {
-        Gajipokok = e;
-    }
-    public void setTunjangan(int r) {
-        Tunjangan = r;
-    }
-    public void setBonus(int t) {
-        Bonus = t;
-    }
-    public void setDenda(int y) {
-        Denda = y;
-    }
-    public void setNamaKar(String w) {
-        NamaKar = w;
-    }
-    //get
-    public int getNIK() {
-        return NIK;
-    }
-    public int getGajipokok() {
-        return Gajipokok;
-    }
-    public int getTunjangan() {
-        return Tunjangan;
-    }
-    public int getBonus() {
-        return Bonus;
-    }
-    public int getDenda() {
-        return Denda;
-    }
-    public String getNamaKar() {
-        return NamaKar;
-    }
-    public void dis(){
-        System.out.println("NIK        : "+NIK);
-        System.out.println("Nama       : "+NamaKar);
-        System.out.println("Gaji Pokok : "+Gajipokok);
-        System.out.println("Tunjangan  : "+Tunjangan);
-        System.out.println("Bonus      : "+Bonus);
-        System.out.println("Denda      : "+Denda);
-        
-    
-    
+//Membuat array 1 dimensi 
+    private String[] Nama={"Paijo","Lala","Lulu","Sabar","Sule","Paiman","Ponimin","Sutiman","Raja","Paijo"};
+//Membuat aarray 2 dimensi    
+    private int[][]DataGAJI={
+        {101, 2000, 1000, 500, 200},
+        {102, 1750, 900, 500, 200},
+        {103, 2150, 1000, 300, 375},
+        {104, 1500, 750, 600, 200},
+        {105, 2500, 850, 500, 300},
+        {106, 1500, 350, 450, 250},
+        {107, 2000, 1000, 500, 200},
+        {108, 1500, 1250, 500, 300},
+        {109, 1000, 1000, 500, 200},
+        {110, 2250, 1000, 200, 200}
+    };
+    //Menambah method get untuk semua data
+public void get(){
+    System.out.println("\t                 **Semua Data Karyawan**");
+    System.out.println("");
+    System.out.printf("%s %9s %17s %15s %12s %15s \n",
+    "NO","Nik","Nama","Gaji Pokok","Tunjangan","Bonus","Denda");
+    //Nama
+        for(int i=0;i<Nama.length;i++){
+            System.out.print(DataGAJI[i][0]+"\t");
+            System.out.print(Nama[i]+"\t\t");
+    //Data Gaji
+        for (int j=1;j<5;j++){
+                System.out.print(DataGAJI[i][j]+"\t\t");
+            }System.out.println();
+        }System.out.println("\n");
     }
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
--- src/MainData.java
+++ src/MainData.java
@@ -1,18 +1,10 @@
 public class MainData {
     public static void main(String[] args) {
-        Karyawan pa = new Karyawan ();
-        pa.Karyawan(101, "Paijo", 2000, 1000, 500, 200);
-        pa.Karyawan(102, "Lala", 1750, 900, 500, 200);
-        pa.Karyawan(103, "Lulu", 2150, 1000, 300,375);
-        pa.Karyawan(104, "Sabar", 1500, 750, 600, 200);
-        pa.Karyawan(105, "Sule", 2500, 850, 500, 300);
-        pa.Karyawan(106, "Paiman", 1500, 350, 450, 250);
-        pa.Karyawan(107, "Ponimin", 2000, 1000, 500, 200);
-        pa.Karyawan(108, "Sutiman", 1500, 1250, 500, 300);
-        pa.Karyawan(109,"Raja", 1000, 1000, 500, 200);
-        pa.Karyawan(110,"Harry", 2250, 1000, 200, 200);
+    Karyawan NY = new Karyawan();
+    //pemanggilan Semua data
+    NY.get();
         
-        //Semua Data
-        System.out.println("FULL DATA");
+        
+        
 }
 }
