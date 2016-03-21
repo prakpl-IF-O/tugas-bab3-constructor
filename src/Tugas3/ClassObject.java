b207bc4fedc28e2c8c9e17e81e35d67b893b89d5
Khrisna WIdhi <khrisnaww@gmail.com>
2016-03-21 21:07:56 +0700

    telah membuat 10 ArrayList

--- src/Tugas3/ClassObject.java
+++ src/Tugas3/ClassObject.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Tugas3;
-
-/**
- *
- * @author Khrisna
- */
 public class ClassObject {
+    private String nik;
+    private String nama;
+    private double gaji_pokok;
+    private double tunjangan;
+    private double bonus;
+    private double denda; 
     
+    public ClassObject(){
+    
+    }
+    
+    public ClassObject(String NIK,String Nama,double GajiPokok,double Tunjangan,double Bonus,double Denda){
+    this.nik=NIK;
+    this.nama=Nama;
+    this.gaji_pokok=GajiPokok;
+    this.tunjangan=Tunjangan;
+    this.bonus=Bonus;
+    this.denda=Denda; 
+    }
 }
