d5cbb573261bb2b0284213e904536d7cc8d81592
Yan Aditiya Nugraha <yanaditiya29@gmail.com>
2016-03-21 07:18:38 +0700

    Pendeklarasian costructor

---
+++ src/tugas2/Karyawan.java
@@ -0,0 +1,16 @@
+
+package tugas2;
+
+public class Karyawan {
+    private String nama;
+    private int nik,gajiPokok,tunjangan,bonus,denda;
+    
+    public Karyawan(String x,int a,int b,int c,int d,int e){
+        this.nama=x;
+        this.denda=a;
+        this.gajiPokok=b;
+        this.nik=c;
+        this.tunjangan=e;
+        this.bonus=d;
+    }
+}
