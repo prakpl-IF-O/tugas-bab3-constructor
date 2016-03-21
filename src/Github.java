63282b4489a929e35092834aede67bbf9af758ee
Meidita <meiditafamirah@yahoo.com>
2016-03-21 21:42:06 +0700

    manggil get sm get id

---
+++ src/Github.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Meidita
+ */
+public class Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  Maingithub db = new Maingithub() ;
+       
+      db.get();
+      db.getId(104);
+      db.getF("gaji","<",200); //erorr
+      
+      } 
+         
+}
+         
+         
+         
+         
+         
+  
+    
+    
+
