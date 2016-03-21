1f479955f5c360c22bae5b3c4c93157a5440a0aa
Enggar Pratama Kurniawan <itsenggar@gmail.com>
2016-03-21 13:59:57 +0700

    mengisi data di array

--- src/DATA_MAIN.java
+++ src/DATA_MAIN.java
@@ -11,6 +11,15 @@
 public class DATA_MAIN {
      public static void main(String[] args) {
         DB db = new DB();
-        
+        db.pendataan(101, "paijo",2000, 1000 , 500 , 200 );
+        db.pendataan(102, "lala", 1750 , 900 , 500 , 200 );
+        db.pendataan(103, "lulu", 2150 , 1000 , 300 , 375 );
+        db.pendataan(104, "sabar", 1500 , 750 , 600 , 200 );
+        db.pendataan(105, "sule", 2500 , 850 , 500 , 300 );
+        db.pendataan(106, "paiman", 1500 , 350 , 450 , 250 );
+        db.pendataan(107, "ponimin", 2000 , 1000 , 500 , 200 );
+        db.pendataan(108, "sutiman", 1500 , 1250 , 500 , 300 );
+        db.pendataan(109, "raja", 1000 , 1000 , 500 , 200 );
+        db.pendataan(110, "harry", 2250 , 1000 , 200 , 200 );
     }
 }
--- src/DB.java
+++ src/DB.java
@@ -15,9 +15,16 @@ public class DB {
     private int[] tunjangan = new int [10];
     private int[] bonus = new int [10];
     private int[] denda = new int [10];
-    
+    int urutan =0;
     public void pendataan (int nik1 , String nama1, int g_pokok1, int tunjangan1, int bonus1
             , int denda1 ){
-        
+        nik[urutan] = nik1 ;
+        nama[urutan]= nama1;
+        g_pokok[urutan] = g_pokok1;
+        tunjangan[urutan] = tunjangan1;
+        bonus[urutan]=bonus1;
+        denda[urutan]=denda1;
+    urutan++;
     }
+    
 }
