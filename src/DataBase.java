115e7c261ed456c769071567495fd05eae70e25f
Katherine Ivana Ruslim <katherineruslim@yahoo.co.id>
2016-03-21 10:09:42 +0700

    Membuat class dengan nama DataBase serta memasukkan data Karyawan

---
+++ src/DataBase.java
@@ -0,0 +1,12 @@
+package database;
+public class DataBase {
+    private int   [] nik       = {101    ,102   ,103   ,104    ,105   ,106      ,107      ,108      ,109   ,110    };
+    private String[] nama      = {"Paijo","Lala","Lulu","Sabar","Sule","Paiman" ,"Ponimin","Sutiman","Raja","Harry"};
+    private int   [] gajiPokok = {2000   ,1750  ,2150  ,1500   ,2500  ,1500     ,2000     ,1500     ,1000  ,2250   };
+    private int   [] tunjangan = {1000   ,900   ,1000  ,750    ,850   ,350      ,1000     ,1250     ,1000  ,1000   };
+    private int   [] bonus     = {500    ,500   ,300   ,600    ,500   ,450      ,500      ,500      ,500   ,200    };
+    private int   [] denda     = {200    ,200   ,375   ,200    ,300   ,250      ,200      ,300      ,200   ,200    };
+    
+    
+
+}
