f7e962102e6315a316430436f729028e91c57a2e
Katherine Ivana Ruslim <katherineruslim@yahoo.co.id>
2016-03-21 11:51:48 +0700

    Menambahkan method get dengan 2 parameter yaitu columnName yang bertipe data String dan value yang bertipe data Integer

--- src/DataBase.java
+++ src/DataBase.java
@@ -37,6 +37,60 @@ public class DataBase {
         System.out.println(" ");
     }
     
+    public void get(String columnName, int value){
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("\t \tDATA BASE KARYAWAN");
+        System.out.println("-----------------------------------------------------------");
+        if (columnName == "Gaji Pokok"){
+            System.out.println("DATA KARYAWAN YANG MEMILIKI GAJI POKOK : " +value);
+            System.out.println("-----------------------------------------------------------");
+            System.out.printf("%-7s%-10s%-13s%-13s%-10s%-6s\n","NIK","Nama","Gaji Pokok","Tunjangan","Bonus","Denda");
+            for (int i=0; i<gajiPokok.length; i++){
+                if ( value == gajiPokok[i]){
+                System.out.printf("%-7d%-13s%-12d%-12d%-10d%-6d\n", nik[i],nama[i],gajiPokok[i],tunjangan[i],bonus[i],denda[i]);
+                } else if (value > 2500){ System.out.printf("%-7s%-13s%-12s%-12s%-10s%-6s\n","-","-","-","-","-","-");break; 
+                } else if (value < 2000){ System.out.printf("%-7s%-13s%-12s%-12s%-10s%-6s\n","-","-","-","-","-","-");break;}
+            } 
+            
+        } else if (columnName == "Tunjangan"){
+            System.out.println("DATA KARYAWAN YANG MEMILIKI TUNJANGAN : " +value);
+            System.out.println("-----------------------------------------------------------");
+            System.out.printf("%-7s%-10s%-13s%-13s%-10s%-6s\n","NIK","Nama","Gaji Pokok","Tunjangan","Bonus","Denda");
+            for (int i=0; i<tunjangan.length; i++){
+                if ( value == tunjangan[i]){
+                System.out.printf("%-7d%-13s%-12d%-12d%-10d%-6d\n", nik[i],nama[i],gajiPokok[i],tunjangan[i],bonus[i],denda[i]);
+                } else if ( value > 1250 ){ System.out.printf("%-7s%-13s%-12s%-12s%-10s%-6s\n","-","-","-","-","-","-");break;  
+                } else if ( value < 350) { System.out.printf("%-7s%-13s%-12s%-12s%-10s%-6s\n","-","-","-","-","-","-");break; }
+            } 
+        
+        } else if (columnName == "Bonus"){
+            System.out.println("DATA KARYAWAN YANG MEMILIKI BONUS : " +value);
+            System.out.println("----------------------------------------------------------");
+            System.out.printf("%-7s%-10s%-13s%-13s%-10s%-6s\n","NIK","Nama","Gaji Pokok","Tunjangan","Bonus","Denda");
+            for (int i=0; i<bonus.length; i++){
+                if ( value == bonus[i]){
+                System.out.printf("%-7d%-13s%-12d%-12d%-10d%-6d\n", nik[i],nama[i],gajiPokok[i],tunjangan[i],bonus[i],denda[i]);
+                } else if ( value > 600) { System.out.printf("%-7s%-13s%-12s%-12s%-10s%-6s\n","-","-","-","-","-","-");break;
+                } else if ( value < 200) { System.out.printf("%-7s%-13s%-12s%-12s%-10s%-6s\n","-","-","-","-","-","-");break;}
+            }
+            
+        }else if (columnName == "Denda"){
+            System.out.println("DATA KARYAWAN YANG MEMILIKI DENDA : " +value);
+            System.out.println("-----------------------------------------------------------");
+            System.out.printf("%-7s%-10s%-13s%-13s%-10s%-6s\n","NIK","Nama","Gaji Pokok","Tunjangan","Bonus","Denda");
+            for (int i=0; i<denda.length; i++){
+                if ( value == denda[i]){
+                System.out.printf("%-7d%-13s%-12d%-12d%-10d%-6d\n", nik[i],nama[i],gajiPokok[i],tunjangan[i],bonus[i],denda[i]);
+                } else if (value > 375) { System.out.printf("%-7s%-13s%-12s%-12s%-10s%-6s\n","-","-","-","-","-","-");break;
+                } else if (value < 200) { System.out.printf("%-7s%-13s%-12s%-12s%-10s%-6s\n","-","-","-","-","-","-");break;} 
+            } 
+        }
+        System.out.println("-----------------------------------------------------------");
+        System.out.println(" ");
+    }
+    
+    
+    
     
     
     
