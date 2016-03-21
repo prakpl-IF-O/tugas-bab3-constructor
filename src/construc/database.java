8dc7092d1c44465baff723a5b278efdc38b0b8e2
Muhammad Bayu Lesmana <bayyulesmana22@gmail.com>
2016-03-21 22:01:17 +0700

    membuat methodd

--- src/construc/database.java
+++ src/construc/database.java
@@ -45,4 +45,11 @@ public class database {
             }
         }
 
-   
+        if (columnName.equals("2")) {
+            System.out.println("NIK     NAMA    GajiPokok   Tunjangan   Bonus   Denda");
+            for (int i = 0; i < 10; i++) {
+                if (tunjangan[i] == value) {
+                    System.out.printf("%-7d %-9s %-11d %-10d %-8d %d\n", nik[i], nama[i], gajiPokok[i], tunjangan[i], bonus[i], denda[i]);
+                }
+            }
+        }
