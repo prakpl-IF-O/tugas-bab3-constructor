0912781455caa14694435cb7c505ace4e41f299a
Yan Aditiya Nugraha <yanaditiya29@gmail.com>
2016-03-21 07:29:33 +0700

    Inisialisasi Constructor

---
+++ src/tugas2/mainKaryawan.java
@@ -0,0 +1,19 @@
+
+package tugas2;
+
+
+public class mainKaryawan {
+    public static void main(String[] args) {
+        Karyawan paijo=new Karyawan("Paijo",200,2000,101,500,1000);
+        Karyawan lala=new Karyawan("Lala",200,1750,102,500,900);
+        Karyawan lulu=new Karyawan("Lulu",375,2150,103,300,1000);
+        Karyawan sabar=new Karyawan("Sabar",200,1500,104,600,750);
+        Karyawan sule=new Karyawan("Sule",300,2500,105,500,850);
+        Karyawan paiman=new Karyawan("Paiman",250,1500,106,450,350);
+        Karyawan ponimin=new Karyawan("Ponimin",200,2000,107,500,1000);
+        Karyawan sutiman=new Karyawan("Sutiman",300,1500,108,500,1250);
+        Karyawan raja=new Karyawan("Raja",200,1000,109,500,1000);
+        Karyawan harry=new Karyawan("Harry",200,2250,110,200,1000);
+        
+    }
+}
