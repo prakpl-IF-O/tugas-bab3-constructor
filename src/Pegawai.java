1ea1d9d2973ea37759a7c5549fdb5fbbefbd05e6
Heru apriadi <heruapr@gmail.com>
2016-03-21 21:22:40 +0700

    final touch ,selesai

--- src/Pegawai.java
+++ src/Pegawai.java
@@ -31,10 +31,57 @@ public class Pegawai {
                 System.out.println("bonus :"+bonuss[i]);
                 System.out.println("denda :"+dendaa[i]);
             }
-        
+                }    
+            }
+        public Pegawai(String a,double b){
+            for(int i=0;i<10;i++){
+                if(a.equals("gaji") && b==gajip[i]){
+                System.out.println("nik :"+nik1[i]);
+                System.out.println("nama :"+nama1[i]);
+                System.out.println("gaji pokok :"+gajip[i]);
+                System.out.println("tunjangan :"+tun[i]);
+                System.out.println("bonus :"+bonuss[i]);
+                System.out.println("denda :"+dendaa[i]);
+                    System.out.println("=============");   
                 }
-            public Pegawai()
+            }
+           }
+              public Pegawai(String c, String d,double e){
+                  for(int i=0;i<10;i++){
+                      for(int j=0;j<10;j++){
+                  
+                  if(c.equals("tunjangan")&& d.equals("<=")&&e==tun[i]){
+                while(tun[j]<=tun[i]){  
+                System.out.println("nik :"+nik1[j]);
+                System.out.println("nama :"+nama1[j]);
+                System.out.println("gaji pokok :"+gajip[j]);
+                System.out.println("tunjangan :"+tun[j]);
+                System.out.println("bonus :"+bonuss[j]);
+                System.out.println("denda :"+dendaa[j]);
+                System.out.println("=============");break;
+                }
+                      }
+                    }
+              }
+            }    
+              public Pegawai (String bo,char op, double bon){
+                  for(int i=0;i<10;i++){
+                      for(int j=0;j<10;j++){
+                      if(bo.equals("bonus") && op=='>' && bon==bonuss[i]){
+                while(bonuss[j]>bonuss[i]){          
+                System.out.println("nik :"+nik1[j]);
+                System.out.println("nama :"+nama1[j]);
+                System.out.println("gaji pokok :"+gajip[j]);
+                System.out.println("tunjangan :"+tun[j]);
+                System.out.println("bonus :"+bonuss[j]);
+                System.out.println("denda :"+dendaa[j]);
+                    System.out.println("=============");  break;  
+                      }
+                     }
+                  }
+                 }
+              }   
     }
 
-}
+
             
--- src/PegawaiMain.java
+++ src/PegawaiMain.java
@@ -1,7 +1,10 @@
 public class PegawaiMain {
  public static void main(String[] args) {
    //Pegawai db = new Pegawai();
-   Pegawai db2 = new Pegawai(104);
+   //Pegawai db2 = new Pegawai(104);
+     //Pegawai db3=new Pegawai("gaji",2000);
+    // Pegawai db4=new Pegawai("tunjangan","<=",900);
+     Pegawai db5=new Pegawai("bonus",'>',450);
 }
    
 }
