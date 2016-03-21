aad305e56bb1d85b45fa3e40d4f7513ea27d6a46
Katherine Ivana Ruslim <katherineruslim@yahoo.co.id>
2016-03-21 10:51:08 +0700

    Menambahkan method get dengan parameter id yang bertipe data integer untuk menampilkan data karyawan yang memiliki id tersebut

--- src/DataBase.java
+++ src/DataBase.java
@@ -8,17 +8,37 @@ public class DataBase {
     private int   [] denda     = {200    ,200   ,375   ,200    ,300   ,250      ,200      ,300      ,200   ,200    };
     
     public void get(){
-        System.out.println("--------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------");
         System.out.println("\t \t DATA BASE KARYAWAN");
-        System.out.println("--------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------");
         System.out.printf("%-7s%-10s%-13s%-13s%-10s%-6s\n","NIK","Nama","Gaji Pokok","Tunjangan","Bonus","Denda");
         for (int i=0; i<nik.length; i++){
             System.out.printf("%-7d%-13s%-12d%-12d%-10d%-6d", nik[i],nama[i],gajiPokok[i],tunjangan[i],bonus[i],denda[i]);
             System.out.println(" ");
         }
-        System.out.println("--------------------------------------------------------");
+        System.out.println("-----------------------------------------------------------");
         System.out.println(" ");
     }
+    
+    public void get (int id){
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("\t \t DATA BASE KARYAWAN");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("DATA KARYAWAN YANG MEMILIKI ID : "+id);
+        System.out.println("-----------------------------------------------------------");
+        
+        for (int i=0; i<nik.length; i++){
+            if (id==nik[i]){
+                System.out.printf("%-7s%-10s%-13s%-13s%-10s%-6s\n","NIK","Nama","Gaji Pokok","Tunjangan","Bonus","Denda");
+                System.out.printf("%-7d%-13s%-12d%-12d%-10d%-6d\n", nik[i],nama[i],gajiPokok[i],tunjangan[i],bonus[i],denda[i]);
+            }
+        }
+        System.out.println("-----------------------------------------------------------");
+        System.out.println(" ");
+    }
+    
+    
+    
     
 
 }
