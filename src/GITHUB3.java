613b2245df98d33f64335c20d9e0bb2b37fdaeee
mfnaim <naimfattahp@gmail.com>
2016-03-21 21:53:03 +0700

    Update GITHUB3.java
    menambahkan method

--- src/GITHUB3.java
+++ src/GITHUB3.java
@@ -35,4 +35,78 @@ public class GITHUB3 {
         }
 
     }
+    
+    public static void Db() {
+        int NIK1 = 100, NIK2 = 200, NIK3 = 300, NIK4 = 400, NIK5 = 500, NIK6 = 600, NIK7 = 700;
+        String Nama1 = "Andi", Nama2 = "Ando", Nama3 = "Andu", Nama4 = "Anda", Nama5 = "Putri", Nama6 = "Emma", Nama7 = "Watson";
+        int GP1 = 1000, GP2 = 1023, GP3 = 982, GP4 = 1439, GP5 = 2102, GP6 = 2000, GP7 = 1093;
+        int GT1 = 870, GT2 = 670, GT3 = 300, GT4 = 960, GT5 = 1200, GT6 = 900, GT7 = 400;
+        int Bon1 = 200, Bon2 = 210, Bon3 = 220, Bon4 = 230, Bon5 = 240, Bon6 = 250, Bon7 = 260;
+        int Den1 = 50, Den2 = 40, Den3 = 50, Den4 = 95, Den5 = 20, Den6 = 15, Den7 = 45;
+
+        System.out.println("Nik : " + NIK1);
+        System.out.println("Nama : " + Nama1);
+        System.out.println("Gaji Pokok : " + GP1);
+        System.out.println("Gaji Tunjangan : " + GT1);
+        System.out.println("Bonus : " + Bon1);
+        System.out.println("Denda : " + Den1);
+
+        System.out.println();
+        System.out.println("Nik : " + NIK2);
+        System.out.println("Nama : " + Nama2);
+        System.out.println("Gaji Pokok : " + GP2);
+        System.out.println("Gaji Tunjangan : " + GT2);
+        System.out.println("Bonus : " + Bon2);
+        System.out.println("Denda : " + Den2);
+        System.out.println();
+        System.out.println("Nik : " + NIK3);
+        System.out.println("Nama : " + Nama3);
+        System.out.println("Gaji Pokok : " + GP3);
+        System.out.println("Gaji Tunjangan : " + GT3);
+        System.out.println("Bonus : " + Bon3);
+        System.out.println("Denda : " + Den3);
+        System.out.println();
+        System.out.println("Nik : " + NIK4);
+        System.out.println("Nama : " + Nama4);
+        System.out.println("Gaji Pokok : " + GP4);
+        System.out.println("Gaji Tunjangan : " + GT4);
+        System.out.println("Bonus : " + Bon4);
+        System.out.println("Denda : " + Den4);
+        System.out.println();
+        System.out.println("Nik : " + NIK5);
+        System.out.println("Nama : " + Nama5);
+        System.out.println("Gaji Pokok : " + GP5);
+        System.out.println("Gaji Tunjangan : " + GT5);
+        System.out.println("Bonus : " + Bon5);
+        System.out.println("Denda : " + Den5);
+        System.out.println();
+        System.out.println("Nik : " + NIK6);
+        System.out.println("Nama : " + Nama6);
+        System.out.println("Gaji Pokok : " + GP6);
+        System.out.println("Gaji Tunjangan : " + GT6);
+        System.out.println("Bonus : " + Bon6);
+        System.out.println("Denda : " + Den6);
+        System.out.println();
+        System.out.println("Nik : " + NIK7);
+        System.out.println("Nama : " + Nama7);
+        System.out.println("Gaji Pokok : " + GP7);
+        System.out.println("Gaji Tunjangan : " + GT7);
+        System.out.println("Bonus : " + Bon7);
+        System.out.println("Denda : " + Den7);
+        System.out.println();
+    }
+
+    public static void DbNIK() {
+        int NIK1 = 100, NIK2 = 200, NIK3 = 300, NIK4 = 400, NIK5 = 500, NIK6 = 600, NIK7 = 700;
+        String Nama1 = "Andi", Nama2 = "Ando", Nama3 = "Andu", Nama4 = "Anda", Nama5 = "Putri", Nama6 = "Emma", Nama7 = "Watson";
+
+        System.out.println("Nik : " + NIK1);
+        System.out.println("Nik : " + NIK2);
+        System.out.println("Nik : " + NIK3);
+        System.out.println("Nik : " + NIK4);
+        System.out.println("Nik : " + NIK5);
+        System.out.println("Nik : " + NIK6);
+        System.out.println("Nik : " + NIK7);
+
+    }
 }
