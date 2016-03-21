d526f7d24970c6ef1488f6037b5ea7ad8f161403
mahendraziref <zirefb@gmail.com>
2016-03-21 17:16:10 +0900

    tinggal ngedit outputan

--- tugas_3/src/tugas_3/tugas_3A.java
+++ tugas_3/src/tugas_3/tugas_3A.java
@@ -10,14 +10,17 @@ package tugas_3;
  * @author Zyref
  */
 public class tugas_3A {
+
     public static void main(String[] args) {
-    
-     Tugas_3 A = new Tugas_3 ();
-     
-     A.Datagaji();
-     A.DataX(109);
-     
-     
-     }  
+
+        Tugas_3 A = new Tugas_3();
+
+        A.Datagaji();
+        A.DataX(109);
+        A.gaji1500("Gaji Pokok", 1500);
+        A.Data("Tunjangan", ">=", 1000);
+        A.Data("Denda", ">", 200);
+        System.out.println("Akhirnya selesai");
+
     }
-     
+}
