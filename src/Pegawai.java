c507f9b9eb7969866d9f57335f6444d9dfd44ef1
Heru apriadi <heruapr@gmail.com>
2016-03-21 19:44:28 +0700

    deklarasi array data pegawai awal

---
+++ src/Pegawai.java
@@ -0,0 +1,16 @@
+public class Pegawai {
+    int nik;
+    double gajiPokok,tunjangan,bonus,denda;
+    String nama;
+    int []nik1={101,102,103,104,105,106,107,108,109,110};
+    String []nama1={"paijo","lala","lulu","sabar","sule",
+                    "paiman","poinimin","sutiman","raja","hari"};
+    double[]gajip ={2000,1750,2150,1500,2500,1500,2000,1500,1000,2250};
+    double[]tun={1000,900,1000,750,850,350,1000,1250,1000,1000};
+    double[]bonuss={500,500,300,600,500,450,500,500,500,200};
+    double[]dendaa={200,200,375,200,300,250,200,300,200,200};
+    
+    
+
+}
+            
