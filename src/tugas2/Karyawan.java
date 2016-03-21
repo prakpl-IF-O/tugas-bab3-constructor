28923eff348e96fc5e5cb7f6bbd6b410e597c722
Yan Aditiya Nugraha <yanaditiya29@gmail.com>
2016-03-21 21:13:28 +0700

    Menggunakan array List pada class Karyawan untuk menyimpan objek agar dapat diproses

--- src/tugas2/Karyawan.java
+++ src/tugas2/Karyawan.java
@@ -1,16 +1,56 @@
 
 package tugas2;
-
+import java.util.ArrayList;
 public class Karyawan {
     private String nama;
     private int nik,gajiPokok,tunjangan,bonus,denda;
     
-    public Karyawan(String x,int a,int b,int c,int d,int e){
-        this.nama=x;
-        this.denda=a;
-        this.gajiPokok=b;
-        this.nik=c;
-        this.tunjangan=e;
-        this.bonus=d;
+    ArrayList<Karyawan>listkaryawan;
+    
+    public Karyawan(){
+        listkaryawan= new ArrayList<Karyawan>();
     }
+    
+    public ArrayList<Karyawan>getAll(){
+        return listkaryawan;
+    }
+    
+    public void masukObjekkeArray(Karyawan ... z){
+        for(int i =0;i<z.length;i++){
+            listkaryawan.add(z[i]);
+        }
+    }
+   
+     public void get(){
+        for(Karyawan total : listkaryawan ){
+            System.out.println("===== DATA KARYAWAN =====");
+            total.tampilData();
+        }
+    }
+     
+    public Karyawan(String nama,int den,int gapok,int nik,int bon,int tun){
+        this.nama=nama;
+        this.denda=den;
+        this.gajiPokok=gapok;
+        this.nik=nik;
+        this.tunjangan=tun;
+        this.bonus=bon;
+    }
+    
+    public void tampilData(){
+        System.out.println("Data Karyawaan : ");
+        System.out.println("NIK : "+this.nik);
+        System.out.println("Nama : "+this.nama);
+        System.out.println("Gaji Pokok : "+this.gajiPokok);
+        System.out.println("Tunjangan : "+this.tunjangan);
+        System.out.println("Bonus : "+this.bonus);
+        System.out.println("Denda : "+this.denda);
+        System.out.println("======================================");
+    }
+    
+    public void get(int x){
+        System.out.println("NIK : "+x);
+        System.out.println("Nama : "+this.nama);
+    }
+    
 }
--- src/tugas2/mainKaryawan.java
+++ src/tugas2/mainKaryawan.java
@@ -1,19 +1,22 @@
-
 package tugas2;
 
+import java.util.ArrayList;
 
 public class mainKaryawan {
+
     public static void main(String[] args) {
-        Karyawan paijo=new Karyawan("Paijo",200,2000,101,500,1000);
-        Karyawan lala=new Karyawan("Lala",200,1750,102,500,900);
-        Karyawan lulu=new Karyawan("Lulu",375,2150,103,300,1000);
-        Karyawan sabar=new Karyawan("Sabar",200,1500,104,600,750);
-        Karyawan sule=new Karyawan("Sule",300,2500,105,500,850);
-        Karyawan paiman=new Karyawan("Paiman",250,1500,106,450,350);
-        Karyawan ponimin=new Karyawan("Ponimin",200,2000,107,500,1000);
-        Karyawan sutiman=new Karyawan("Sutiman",300,1500,108,500,1250);
-        Karyawan raja=new Karyawan("Raja",200,1000,109,500,1000);
-        Karyawan harry=new Karyawan("Harry",200,2250,110,200,1000);
-        
+        prosesData db = new prosesData();
+        db.setObjecttoArray("Paijo", 200, 2000, 101, 500, 1000);
+        db.setObjecttoArray("Lala", 200, 1750, 102, 500, 900);
+        db.setObjecttoArray("Lulu", 375, 2150, 103, 300, 1000);
+        db.setObjecttoArray("Sabar", 200, 1500, 104, 600, 750);
+        db.setObjecttoArray("Sule", 300, 2500, 105, 500, 850);
+        db.setObjecttoArray("Paiman", 250, 1500, 106, 450, 350);
+        db.setObjecttoArray("Ponimin", 200, 2000, 107, 500, 1000);
+        db.setObjecttoArray("Sutiman", 300, 1500, 108, 500, 1250);
+        db.setObjecttoArray("Raja", 200, 1000, 109, 500, 1000);
+        db.setObjecttoArray("Harry", 200, 2250, 110, 200, 1000);
+
+        db.get();
     }
 }
