8d6b9c4b6bac6a25f240ed9afe70be5170bbf834
wahyu dwiky rahmadan <wahyudwiki@gmail.com>
2016-03-21 21:07:52 +0700

    buat pariabel

---
+++ src/git.java
@@ -0,0 +1,11 @@
+public class git {
+String menu [] = {"nama","gaji pokok","nik","tunjangan","bonus","denda"},
+        nik [] = {"1","2","3","4","5","6","7","8","9","10"};
+String nama[] = {"arum","ifa","roro","ayu","puspita","ningrum","anum","apunin","wahyu","dwiki"};
+int gaji [] = {1000,1500,1750,2000,2500,2750,3000,3500,37500,4000};
+int tunjangan[] = {4500,4750,5000,5500,5750,6000,6500,6750,7000,7500};
+int bonus [] = {200,250,275,300,350,375,400,450,475,500};
+int denda [] = {50,60,70,80,90,100,105,110,115,120};    
+
+}
+
