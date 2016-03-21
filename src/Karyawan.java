b74d54d21a14df694973f7a56f59b2b37eb48cd8
Jerry Kurnia Sandy <jerrykurnias@gmail.com>
2016-03-21 14:22:03 +0700

    Pemanggilan denda dan tunjangan selesai dan penambahan Screen Shot

--- src/Karyawan.java
+++ src/Karyawan.java
@@ -1,8 +1,10 @@
+
 public class Karyawan {
 //Membuat array 1 dimensi 
-    private String[] Nama={"Paijo","Lala","Lulu","Sabar","Sule","Paiman","Ponimin","Sutiman","Raja","Paijo"};
+
+    private String[] Nama = {"Paijo", "Lala", "Lulu", "Sabar", "Sule", "Paiman", "Ponimin", "Sutiman", "Raja", "Paijo"};
 //Membuat aarray 2 dimensi    
-    private int[][]DataGAJI={
+    private int[][] DataGAJI = {
         {101, 2000, 1000, 500, 200},
         {102, 1750, 900, 500, 200},
         {103, 2150, 1000, 300, 375},
@@ -15,129 +17,141 @@ public class Karyawan {
         {110, 2250, 1000, 200, 200}
     };
     //Menambah method get untuk semua data
-public void Data(){
-    System.out.println("\t                 **Semua Data Karyawan**");
-    System.out.println("");
-    System.out.printf("%s %9s %17s %15s %12s %15s \n",
-            "Nik","Nama","Gaji Pokok","Tunjangan","Bonus","Denda");
-    //Nama
-        for(int a=0;a<Nama.length;a++){
-            System.out.print(DataGAJI[a][0]+"\t");
-            System.out.print(Nama[a]+"\t\t");
-    //Data Gaji
-        for (int s=1;s<5;s++){
-                System.out.print(DataGAJI[a][s]+"\t\t");
-            }System.out.println();
-        }System.out.println("\n");
+
+    public void Data() {
+        System.out.println("\t                 **Semua Data Karyawan**");
+        System.out.println("");
+        System.out.printf("%s %9s %17s %15s %12s %15s \n",
+                "Nik", "Nama", "Gaji Pokok", "Tunjangan", "Bonus", "Denda");
+        //Nama
+        for (int a = 0; a < Nama.length; a++) {
+            System.out.print(DataGAJI[a][0] + "\t");
+            System.out.print(Nama[a] + "\t\t");
+            //Data Gaji
+            for (int s = 1; s < 5; s++) {
+                System.out.print(DataGAJI[a][s] + "\t\t");
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
     }
 //Mencetak NIK X
-public void Data(int Nik){
-    System.out.println("----------------------------------------------------------------------------");
+
+    public void Data(int Nik) {
+        System.out.println("----------------------------------------------------------------------------");
         System.out.println("\t                     **Data Karyawan X**");
-        for(int a=0;a<Nama.length;a++){
-            if(Nik==DataGAJI[a][0]){
-                System.out.println("Nik\t\t     : "+DataGAJI[a][0]);
-                System.out.println("Nama\t\t     : "+Nama[a]);
-                System.out.println("Gaji Pokok\t     : "+DataGAJI[a][1]);
-                System.out.println("Tunjangan\t     : "+DataGAJI[a][2]);
-                System.out.println("bonus\t\t     : "+DataGAJI[a][3]);
-                System.out.println("denda\t\t     : "+DataGAJI[a][4]);
-            }       
-        }
-}
-//mencetak Gaji Pokok 1500
-public void Data(String Namee, int nilai){
-    System.out.println("");
-    System.out.println("----------------------------------------------------------------------------");
-    System.out.println("\t              **Data Karyawan Gaji 1500**");
-    System.out.printf("%s %9s %17s %15s %12s %15s \n","Nik","Nama","Gaji Pokok","Tunjangan","Bonus","Denda");
-    //Tambahan    
-    int p = 0;
-        if("Gaji Pokok".equalsIgnoreCase(Namee)){
-            p=1;
-        }else if("Tunjangan".equalsIgnoreCase(Namee)){
-            p=2;
-        }else if("Bonus".equalsIgnoreCase(Namee)){
-            p=3;
-        }else if("Denda".equalsIgnoreCase(Namee)){
-            p=4;
-}
-        for(int a=0;a<Nama.length;a++){
-            if(nilai==DataGAJI[a][p]){
-                System.out.print(DataGAJI[a][0]+"\t");
-                System.out.print(Nama[a]+"\t\t");
-                for (int s=1;s<5;s++){
-                    System.out.print(DataGAJI[a][s]+"\t\t");
-                }System.out.println();
+        for (int a = 0; a < Nama.length; a++) {
+            if (Nik == DataGAJI[a][0]) {
+                System.out.println("Nik\t\t     : " + DataGAJI[a][0]);
+                System.out.println("Nama\t\t     : " + Nama[a]);
+                System.out.println("Gaji Pokok\t     : " + DataGAJI[a][1]);
+                System.out.println("Tunjangan\t     : " + DataGAJI[a][2]);
+                System.out.println("bonus\t\t     : " + DataGAJI[a][3]);
+                System.out.println("denda\t\t     : " + DataGAJI[a][4]);
             }
-        }System.out.println("\n");
-}
-//Mencetak tunjangan+denda
-public void Data(String Namee, int nilai, String operator){
-    System.out.println("");
-    System.out.println("----------------------------------------------------------------------------");
-    System.out.println("\t              **Data Karyawan Tunjangan >=1000**");
-    System.out.printf("%s %9s %17s %15s %12s %15s \n","Nik","Nama","Gaji Pokok","Tunjangan","Bonus","Denda");
-    int x = 0;
-        if("Gaji Pokok".equalsIgnoreCase(Namee)){
-            x=1;
-        }else if("Tunjangan".equalsIgnoreCase(Namee)){
-            x=2;
-        }else if("Bonus".equalsIgnoreCase(Namee)){
-            x=3;
-        }else if("Denda".equalsIgnoreCase(Namee)){
-            x=4;
         }
-        switch (operator){
+    }
+//mencetak Gaji Pokok 1500
+
+    public void Data(String Namee, int nilai) {
+        System.out.println("");
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.println("\t              **Data Karyawan Gaji 1500**");
+        System.out.printf("%s %9s %17s %15s %12s %15s \n", "Nik", "Nama", "Gaji Pokok", "Tunjangan", "Bonus", "Denda");
+        //Tambahan    
+        int p = 0;
+        if ("Gaji Pokok".equalsIgnoreCase(Namee)) {
+            p = 1;
+        } else if ("Tunjangan".equalsIgnoreCase(Namee)) {
+            p = 2;
+        } else if ("Bonus".equalsIgnoreCase(Namee)) {
+            p = 3;
+        } else if ("Denda".equalsIgnoreCase(Namee)) {
+            p = 4;
+        }
+        for (int a = 0; a < Nama.length; a++) {
+            if (nilai == DataGAJI[a][p]) {
+                System.out.print(DataGAJI[a][0] + "\t");
+                System.out.print(Nama[a] + "\t\t");
+                for (int s = 1; s < 5; s++) {
+                    System.out.print(DataGAJI[a][s] + "\t\t");
+                }
+                System.out.println();
+            }
+        }
+        
+    }
+//Mencetak tunjangan+denda
+
+    public void Data(String Namee, String operator,int nilai) {
+        System.out.println();
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.println("\t              **Data Karyawan "+Namee+" "+operator+" "+nilai+"**");
+        System.out.printf("%s %9s %17s %15s %12s %15s \n", "Nik", "Nama", "Gaji Pokok", "Tunjangan", "Bonus", "Denda");
+        int x = 0;
+        if ("Gaji Pokok".equalsIgnoreCase(Namee)) {
+            x = 1;
+        } else if ("Tunjangan".equalsIgnoreCase(Namee)) {
+            x = 2;
+        } else if ("Bonus".equalsIgnoreCase(Namee)) {
+            x = 3;
+        } else if ("Denda".equalsIgnoreCase(Namee)) {
+            x = 4;
+        }
+        switch (operator) {
             //Kurang dari
             case "<":
-                for(int a=0;a<Nama.length;a++){
-                    if(DataGAJI[a][x]<nilai){
-                        System.out.print(DataGAJI[a][0]+"\t");
-                        System.out.print(Nama[a]+"\t\t");
-                        for (int s=1;s<5;s++){
-                            System.out.print(DataGAJI[a][s]+"\t\t");
-                        }System.out.println();
+                for (int a = 0; a < Nama.length; a++) {
+                    if (DataGAJI[a][x] < nilai) {
+                        System.out.print(DataGAJI[a][0] + "\t");
+                        System.out.print(Nama[a] + "\t\t");
+                        for (int s = 1; s < 5; s++) {
+                            System.out.print(DataGAJI[a][s] + "\t\t");
+                        }
+                       
                     }
                 }
                 break;
-                //Kurang dari sama dengan
+            //Kurang dari sama dengan
             case "<=":
-                for(int a=0;a<Nama.length;a++){
-                    if(DataGAJI[a][x]<=nilai){
-                        System.out.print(DataGAJI[a][0]+"\t");
-                        System.out.print(Nama[a]+"\t\t");
-                        for (int s=1;s<5;s++){
-                            System.out.print(DataGAJI[a][s]+"\t\t");
-                        }System.out.println();
+                for (int a = 0; a < Nama.length; a++) {
+                    if (DataGAJI[a][x] <= nilai) {
+                        System.out.print(DataGAJI[a][0] + "\t");
+                        System.out.print(Nama[a] + "\t\t");
+                        for (int s = 1; s < 5; s++) {
+                            System.out.print(DataGAJI[a][s] + "\t\t");
+                        }
+                        System.out.println();
                     }
                 }
                 break;
-
-
-
-
-
+            //lebih dari
+            case ">":
+                for (int a = 0; a < Nama.length; a++) {
+                    if (DataGAJI[a][x] > nilai) {
+                        System.out.print(DataGAJI[a][0] + "\t");
+                        System.out.print(Nama[a] + "\t\t");
+                        for (int s = 1; s < 5; s++) {
+                            System.out.print(DataGAJI[a][s] + "\t\t");
+                        }
+                        System.out.println();
+                    }
+                }
+                break;
+            ////lebih dari sama dengan
+            case ">=":
+                for (int a = 0; a < Nama.length; a++) {
+                    if (DataGAJI[a][x] >= nilai) {
+                        System.out.print(DataGAJI[a][0] + "\t");
+                        System.out.print(Nama[a] + "\t\t");
+                        for (int s = 1; s < 5; s++) {
+                            System.out.print(DataGAJI[a][s] + "\t\t");
+                        }
+                        System.out.println();
+                    }
+                }
+                break;
+        }
+        
+    }
 }
-
-
-
-
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/MainData.java
+++ src/MainData.java
@@ -7,6 +7,11 @@ public class MainData {
     NY.Data(109);
      //pemanggilan Gaji 1500
     NY.Data("Gaji Pokok", 1500);
-        
+    //pemanggilan Tunjangan >= 1000
+    NY.Data("Tunjangan",">=",1000);
+    //Pemanggilan Denda > 200
+    NY.Data("Denda",">",200);
+        System.out.println("");
+        System.out.println("\t\t\t\t TERIMA KASIH ");
     }
 }
