3ae6f3671375095dd2c4a3b5815adeb7a0d5d824
mfnaim <naimfattahp@gmail.com>
2016-03-21 21:39:02 +0700

    Create GITHUB3.java
    menambah menu

---
+++ src/GITHUB3.java
@@ -0,0 +1,38 @@
+package github3;
+
+import java.util.Scanner;
+
+public class GITHUB3 {
+
+    public static void main(String[] args) {
+            int pil;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Cari berdasarkan : ");
+        System.out.println("1. NIK");
+        System.out.println("2. NAMA");
+        System.out.println("3. GAJI POKOK");
+        System.out.println("4. GAJI TUNJANGAN");
+        System.out.println("5. BONUS");
+        System.out.println("6. DENDA");
+        System.out.println("7. SEMUA DATA");
+        System.out.print("Pilihan : ");
+        pil = input.nextInt();
+
+        if (pil == 1) {
+           
+        } else if (pil == 2) {
+            
+        } else if (pil == 3) {
+            
+        } else if (pil == 4) {
+            
+        } else if (pil == 5) {
+            
+        } else if (pil == 6) {
+            
+        } else if (pil == 7) {
+           
+        }
+
+    }
+}
