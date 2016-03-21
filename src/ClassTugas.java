54e376e4f9f5dbbdbcfcb36cccd8935114aadd47
Fariz Fathullah Afma <farizafma@yahoo.co.id>
2016-03-21 23:59:51 +0700

    pembuatan mthod pada java

--- src/ClassTugas.java
+++ src/ClassTugas.java
@@ -9,5 +9,47 @@
  * @author Fariz
  */
 public class ClassTugas {
-    
+    private String nama,nik;
+private int gajiPokok,tunjangan,bonus,denda;
+public ClassTugas(String nik, String nama, int gajiPokok, int tunjangan, int bonus, int denda){
+    this.nama = nama;
+    this.nik = nik;
+    this.gajiPokok = gajiPokok;
+    this.tunjangan = tunjangan;
+    this.bonus = bonus;
+    this.denda = denda;
+}
+public void tampilData(){
+    System.out.println("nik : "+nik);
+    System.out.println("nama : "+nama);
+    System.out.println("gajiPokok : "+gajiPokok);
+    System.out.println("tunjangan : "+tunjangan);
+    System.out.println("bonus : "+bonus);
+    System.out.println("denda : "+denda);
+    System.out.println("==================");
+}
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNik() {
+        return nik;
+    }
+
+    public int getGajiPokok() {
+        return gajiPokok;
+    }
+
+    public int getTunjangan() {
+        return tunjangan;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public int getDenda() {
+        return denda;
+    }
 }
