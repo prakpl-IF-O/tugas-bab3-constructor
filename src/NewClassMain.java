c242d8894b0f9b8281a650b2214cfce7d7120ce5
Satria Adhi Kharisma <SAKharisma@gmail.com>
2016-03-21 21:35:38 +0700

    commit dan push file sisa

---
+++ src/NewClassMain.java
@@ -0,0 +1,12 @@
+
+public class NewClassMain {
+
+    public static void main(String[] args) {
+        NewClass nw1 = new NewClass();
+        nw1.get();
+        nw1.get(102);
+        nw1.get("gajipokok", 1750);
+        nw1.get("bonus", "<=", 2000);
+    }
+
+}
