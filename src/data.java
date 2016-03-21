13147f4cdc932f91bac5ea88b29a31f53135c194
muhammadiqbalaulia <iqbalgendon21@gmail.com>
2016-03-21 21:20:02 +0700

    pembuatan constractor

---
+++ src/data.java
@@ -0,0 +1,10 @@
+public class data {
+
+    private String[] nama = new String[10];
+    private int[] nik = new int[10];
+    private int[] gapok = new int[10];
+    private int[] tunjangan = new int[10];
+    private int[] bonus = new int[10];
+    private int[] denda = new int[10];
+    int urut=0;
+    
