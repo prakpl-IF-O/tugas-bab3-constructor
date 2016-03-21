0cb6957520df6d2410cdea5a62e9745b50b7f2a0
Husein Abdulbar <abdulbarhusein@yahoo.com>
2016-03-21 20:10:40 +0700

    finishing

--- src/data.java
+++ src/data.java
@@ -2,37 +2,16 @@
 public class data {
 
     String m[] = {"nik", "nama", "gaji pokok", "tunjangan", "bonus", "denda"},
-            nik[] = {"001", "002", "003", "004", "005", "006", "007", "0008", "009", "010"},
-            n[] = {"Husein", "Tangguh", "Irsyad", "Fitrah", "Raka", "Rizki", "Hafidz", "Izzan", "Andi", "Iqbal"};
+         nik[] = {"001", "002", "003", "004", "005", "006", "007", "0008", "009", "010"},
+           n[] = {"Husein", "Tangguh", "Irsyad", "Fitrah", "Raka", "Rizki", "Hafidz", "Izzan", "Andi", "Iqbal"};
     int g[] = {2000, 2500, 1500, 2000, 1250, 2700, 3000, 2000, 1500, 2500},
-            t[] = {1000, 900, 750, 1000, 800, 1000, 900, 1250, 750, 500},
-            b[] = {500, 500, 300, 600, 500, 450, 500, 500, 500, 200},
-            d[] = {200, 200, 375, 200, 300, 250, 200, 300, 200, 200,};
+        t[] = {1000, 900, 750, 1000, 800, 1000, 900, 1250, 750, 500},
+        b[] = {500, 500, 300, 600, 500, 450, 500, 500, 500, 200},
+        d[] = {200, 200, 375, 200, 300, 250, 200, 300, 200, 200,};
     public void get() {
-        for (int i = 0; i < 6; i++) {
-            System.out.print(m[i] + " ");
-            for (int j = 0; j < 10; j++) {
-                switch (i) {
-                    case 1:
-                        System.out.print(n[j] + " ");
-                        break;
-                    case 2:
-                        System.out.print(g[j] + " ");
-                        break;
-                    case 3:
-                        System.out.print(t[j] + " ");
-                        break;
-                    case 4:
-                        System.out.print(b[j] + " ");
-                        break;
-                    case 5:
-                        System.out.print(d[j] + " ");
-                        break;
-                    case 0:
-                        System.out.print(nik[j] + " ");
-                        break;
-                }
-            }
+        for (int i = 0; i < 10; i++) {
+            
+            sw(i);
             System.out.println("");
         }
         System.out.println("=========================");
