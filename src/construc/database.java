43c6dc2574f8d99f9a1b78fa5b0814f9e9e4f6e9
Muhammad Bayu Lesmana <bayyulesmana22@gmail.com>
2016-03-21 21:52:47 +0700

    membuat array

--- src/construc/database.java
+++ src/construc/database.java
@@ -10,5 +10,13 @@ package construc;
  * @author Win_8.1
  */
 public class database {
-    
+
+    int[] nik = {101, 102, 103, 104, 105, 106, 107, 108, 109, 110};
+    String[] nama = {"Paijo", "Lala", "Lulu", "Sabar", "Sule", "Paiman", "Ponimin", "Sutiman", "Raja", "Hary"};
+    int[] gajiPokok = {2000, 1750, 2150, 1500, 2500, 1500, 2000, 1500, 1000, 2250};
+    int[] tunjangan = {1000, 900, 1000, 850, 750, 350, 1000, 350, 1000, 1000};
+    int[] bonus = {500, 500, 300, 600, 500, 450, 500, 500, 500, 200};
+    int[] denda = {200, 200, 375, 300, 200, 200, 200, 300, 300, 200};
+
 }
+
