db49daf7995255d0af068bd4bcabfdab4de1a475
Satria Adhi Kharisma <SAKharisma@gmail.com>
2016-03-21 14:15:37 +0700

    pembuatan class, instansiasi variable dan buat constructor

---
+++ src/NewClass.java
@@ -0,0 +1,22 @@
+
+public class NewClass {
+
+    public int nik;
+    public String nama;
+    public int gajipokok;
+    public int tunjangan;
+    public int bonus;
+    public int denda;
+
+    public NewClass(int nik, String nama, int gajipokok, int tunjangan, int bonus, int denda) {
+
+        this.nik = nik;
+        this.nama = nama;
+        this.gajipokok = gajipokok;
+        this.tunjangan = tunjangan;
+        this.bonus = bonus;
+        this.denda = denda;
+
+    }
+
+}
