538cd443d1b47d7bc4977244940789d71f60ba46
Meidita <meiditafamirah@yahoo.com>
2016-03-21 18:19:44 +0700

    akhirnyaa bisaa masukin dataaaa~~~~~~~

---
+++ src/Maingithub.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github;
+
+/**
+ *
+ * @author Meidita
+ */
+public class Maingithub {
+    
+  String[]karyawan1={"Nik : 101 ","Nama : paijo ","Gaji pokok :2000 ","tunjangan: 1000 ","bonus : 500","denda :200 "};  
+         
+  String[]karyawan2={"Nik : 102 ","Nama : Lala ","Gaji pokok :1750 ","tunjangan: 900 ","bonus : 500","denda :500 "};  
+     
+  String[]karyawan3={"Nik : 103 ","Nama : Lulu ","Gaji pokok :2150","tunjangan: 1000 ","bonus : 300","denda :375 "};  
+       
+  String[]karyawan4={"Nik : 101 ","Nama : Sabar ","Gaji pokok :1500","tunjangan: 750 ","bonus : 600","denda :200 "};  
+       
+  String[]karyawan5={"Nik: 105","Nama : Sule ","Gaji Pokok : 1500 ","tunjangan : 750","bonus : 600","denda : 200"};
+  
+  String[]karyawan6={"Nik : 106","Nama : Paiman","Gaji Pokok : 2500 ","tunjungan : 860","bonus : 500 "," denda :250"};
+  
+  String[]karyawan7={"Nik: 107","Nama : ponimin","Gaji Pokok : 2000","tunjangan: 1000","bonus: 500","denda : 200"};
+  
+  String[]karyawan8={"Nik: 108 ","Nama: Sutiman ","Gaji Pokok : 1500","tunjangan : 1250","bonus :500","denda : 300"};
+  
+  String[]karyawan9={"Nik :109","Nama: Raja","Gaji Pokok: 1000","tunjangan : 1000","bonus :500","denda :200"};
+
+  String[]karyawan10={"Nik :110","Nama : Harry ","Gaji Pokok : 2250","tunjangan : 1000","bonus :200","denda : 200"};
+  
+     public void get(){
+  
+   for (int i=0; i<karyawan1.length; i++){
+            System.out.println(karyawan1[i]);
+    }
+    
+  for (int i=0; i<karyawan2.length; i++){
+            System.out.println(karyawan2[i]);
+    }  
+    
+  for (int i=0; i<karyawan3.length; i++){
+            System.out.println(karyawan3[i]);
+    }
+    
+  for (int i=0; i<karyawan4.length; i++){
+            System.out.println(karyawan4[i]);
+    }
+  
+   for (int i=0; i<karyawan5.length; i++){
+            System.out.println(karyawan5[i]);
+    }
+     for (int i=0; i<karyawan6.length; i++){
+            System.out.println(karyawan6[i]);
+    }
+      for (int i=0; i<karyawan7.length; i++){
+            System.out.println(karyawan7[i]);
+    }
+       for (int i=0; i<karyawan8.length; i++){
+            System.out.println(karyawan8[i]);
+    }
+        for (int i=0; i<karyawan9.length; i++){
+            System.out.println(karyawan9[i]);
+    }
+         for (int i=0; i<karyawan10.length; i++){
+            System.out.println(karyawan10[i]);
+    }
+    }
+   void getId (){
+       System.out.println("----------------------");
+       System.out.println(karyawan1[1]);
+       System.out.println(karyawan2[1]);
+       System.out.println(karyawan3[1]);
+       System.out.println(karyawan4[1]);
+       System.out.println(karyawan5[1]);
+       System.out.println(karyawan6[1]);
+       System.out.println(karyawan7[1]);
+       System.out.println(karyawan8[1]);
+       System.out.println(karyawan9[1]);
+       System.out.println(karyawan10[1]);
+       System.out.println("---------------------");
+       
+   }   
+   
+   void gett(){
+       System.out.println(karyawan1);
+   }
+     
+  
+     } 
+
+     
